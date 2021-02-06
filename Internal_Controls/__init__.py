e278bb5893a02de535e9848fd5fc427bbb8d9cee
Thierry Haddad <thierry.haddad@sevilleinvestments.ca>
2021-02-05 16:44:12 -0800

    update tabs and upload features

--- Internal_Controls/__init__.py
+++ Internal_Controls/__init__.py
@@ -1,4 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
+app.config['UPLOAD_PATH'] = 'C:\\Users\\ThierryHaddad\\source\\repos\\Internal_Controls\\Internal_Controls\\static\\uploads'
+
+
+
 import Internal_Controls.views
--- Internal_Controls/views.py
+++ Internal_Controls/views.py
@@ -3,8 +3,14 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template, request, redirect, url_for, abort, send_from_directory
+
 from Internal_Controls import app
+import imghdr
+import os
+from werkzeug.utils import secure_filename
+
+
 
 @app.route('/')
 @app.route('/home')
@@ -26,3 +32,47 @@ def contact():
         message='Your contact page.'
     )
 
+@app.route('/about')
+def about():
+    """Renders the contact page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
+
+@app.route('/upload')
+def upload():
+    files = os.listdir(app.config['UPLOAD_PATH'])
+    return render_template('upload.html', files=files)
+
+def validate_image(stream):
+    header = stream.read(512)
+    stream.seek(0)
+    format = imghdr.what(None, header)
+    if not format:
+        return None
+    return '.' + (format if format != 'jpeg' else 'jpg')
+
+@app.errorhandler(413)
+def too_large(e):
+    return "File is too large", 413
+
+
+
+@app.route('/upload', methods=['POST'])
+def upload_files():
+    uploaded_file = request.files['file']
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
+                file_ext != validate_image(uploaded_file.stream):
+            return "Invalid image", 400
+        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+    return '', 204
+
+@app.route('/uploads/<filename>')
+def uploads(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'], filename)
